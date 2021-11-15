0e8cbe3f00b564e8248035954855dcc6900e184e
Delvoid <delvoidltd@gmail.com>
2021-11-15 20:37:46 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+require('express-async-errors')
+
+// express
+const express = require('express')
+
+const app = express()
+// other packages
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+
+// database
+const connectDB = require('./db/connect')
+
+// routes
+// TODO auth
+// TODO users
+
+//middleware
+// TODO auth
+// TODO errorHandler
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(morgan('tiny'))
+
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+
+app.use('/test', (req, res) => {
+  res.send('Test route')
+})
+
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () => console.log(`Server is listening on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
