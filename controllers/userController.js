889993ca499a7544cbebeff01278abc4bb6dcdeb
Delvoid <delvoidltd@gmail.com>
2021-11-18 19:34:42 +0000

    getCurrentUser added, updateUser added, updateUserPassword added, getUserTokens added

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,9 @@ const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 const UserModel = require('../models/User')
 const ObjectId = require('mongoose').Types.ObjectId
+const TokenModel = require('../models/Token')
 
-const {
-  attachCookiesToResponse,
-  createTokenUser,
-  sendVerificationEmail,
-  sendResetPasswordEmail,
-  createHash,
-} = require('../utils')
+const { attachCookiesToResponse, createTokenUser, checkPermissions } = require('../utils')
 
 const getAllUsers = async (req, res) => {
   const users = await UserModel.find({}).select('-password')
@@ -24,13 +19,47 @@ const getUserById = async (req, res) => {
   res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = (req, res) => {
-  res.send('show current user')
+  res.status(StatusCodes.OK).json({ user: req.user })
 }
-const updateUser = (req, res) => {
-  res.send('update user users')
+const updateUser = async (req, res) => {
+  const { name, email } = req.body
+  if (!name || !email) throw new CustomError.BadRequest('Please provide all values')
+  const user = await UserModel.findById(req.user.userId)
+
+  user.email = email
+  user.name = name
+
+  await user.save()
+
+  const tokenUser = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.OK).json({ user: tokenUser })
 }
-const updateUserPassword = (req, res) => {
-  res.send('update password users')
+const updateUserPassword = async (req, res) => {
+  const { oldPassword, newPassword } = req.body
+  if (!oldPassword || !newPassword) throw new CustomError.BadRequest('Please provide both values')
+
+  const user = await UserModel.findById(req.user.userId)
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword)
+  if (!isPasswordCorrect) throw new CustomError.Unauthenticated('Invalid credentials')
+
+  user.password = newPassword
+  await user.save()
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Password Updated.' })
+}
+
+const getUserTokens = async (req, res) => {
+  let { id } = req.params
+  if (!id) id = req.user.userId
+  if (id) {
+    if (!ObjectId.isValid(id)) throw new CustomError.BadRequest('Invalid request')
+  }
+  const tokens = await TokenModel.find({
+    user: id,
+  })
+  res.status(StatusCodes.OK).json({ tokens })
 }
 
 module.exports = {
@@ -39,4 +68,5 @@ module.exports = {
   showCurrentUser,
   updateUser,
   updateUserPassword,
+  getUserTokens,
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,12 +8,16 @@ const {
   showCurrentUser,
   updateUserPassword,
   updateUser,
+  getUserTokens,
+  getUserTokensById,
 } = require('../controllers/userController')
 
 router.get('/', authenticateUser, getAllUsers)
 router.get('/showMe', authenticateUser, showCurrentUser)
 router.patch('/updateUser', authenticateUser, updateUser)
 router.patch('/updateUserPassword', authenticateUser, updateUserPassword)
+
+router.get(['/tokens', '/tokens/:id'], authenticateUser, getUserTokens)
 
 router.get('/:id', authenticateUser, getUserById)
 
